5d840d9cc88d3dc0ad1c9151af84f377cbe81bcb
RenzoDavila27 <renzo.davila27@gmail.com>
2024-09-16 02:21:40 -0300

    JavaDoc Terminado

--- codeJavaDoc/Colocable.java
+++
@@ -1,6 +0,0 @@
-/**
- * Interface de Colocables en el mapa
- */
-public interface Colocable{ //Esto lo hacemos por si en un futuro queremos implementar diferentes formas de posicionamiento de barcos
-    void colocar(Jugador p1, Posicion position, int option);
-}
